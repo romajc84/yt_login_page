bc9bc5e81373a214ed2d3eb05733cbcbc64f3a94
romajc84 <joeyroman84@gmail.com>
2021-06-25 16:24:47 -0400

    yt 33:00

--- lib/main.dart
+++ lib/main.dart
@@ -21,9 +21,47 @@ class LoginPage extends StatefulWidget {
   State createState() => LoginPageState();
 }
 
-class LoginPageState extends State<LoginPage> {
+class LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _iconAnimationController;
+  late Animation<double> _iconAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _iconAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+    _iconAnimation = CurvedAnimation(
+        parent: _iconAnimationController, curve: Curves.bounceOut);
+    _iconAnimation.addListener(() => this.setState(() {}));
+    _iconAnimationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.greenAccent,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image(
+            image: AssetImage('assets/images/girl.jpg'),
+            fit: BoxFit.cover,
+            color: Colors.black87,
+            colorBlendMode: BlendMode.darken,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FlutterLogo(
+                size: 100.0,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
